318fe624c3e14d65405a53de62ee32a19c4f0556
Akshay kumar <akshaychalla08@gmail.com>
2024-07-20 23:12:46 +0530

    first commit

---
+++ learnpython/learn_with_Mosh/string_methods.py
@@ -0,0 +1,13 @@
+course = 'Python for beginners'
+print(len(course))
+'''
+diff btw functions and methods -- when a function is belongs to the specific to some kind of object like string,
+then we ref to that function as a method 
+ex : upper() - this function is valid only for strings
+whereas -  len, print are general purpose and are referred as funtions
+'''
+
+print(course.upper())
+
+
+
